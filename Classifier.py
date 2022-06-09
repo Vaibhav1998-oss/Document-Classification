578196544341e3e4d6ab20f061ea30304b74d56d
Vaibhav1998-oss <99330703+Vaibhav1998-oss@users.noreply.github.com>
2022-06-09 17:00:44 +0530

    Add files via upload

---
+++ Classifier.py
@@ -0,0 +1,191 @@
+from sqlite3 import Cursor
+from random import choice 
+import streamlit as st
+import pandas as pd
+import base64,random
+import time,datetime
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+import io,random
+from streamlit_tags import st_tags
+from PIL import Image
+import pymysql
+import plotly.express as px
+from pyresparser import ResumeParser
+import os
+import spacy
+nlp = spacy.load('en_core_web_sm')
+from docx import Document
+
+def get_table_download_link(df,filename,text):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    # href = f'<a href="data:file/csv;base64,{b64}">Download Report</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    return href
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+connection = pymysql.connect(host='localhost',user='root',password='@Manoj2001',db='manojdb')
+cursor = connection.cursor()
+
+def insert_data(name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses):
+    DB_table_name = 'user_data'
+    insert_sql = "insert into " + DB_table_name + """
+    values (0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    rec_values = (name, email, str(res_score), timestamp,str(no_of_pages), reco_field, cand_level, skills,recommended_skills,courses)
+    cursor.execute(insert_sql, rec_values)
+    connection.commit()
+
+st.set_page_config(
+   page_title="Smart Resume Analyzer",
+   page_icon='Downloads/SRA_Logo.ico',
+)
+
+def run():
+    st.title("Smart Resume Analyser")
+    st.sidebar.markdown("# Choose User")
+    activities = ["Normal User", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+    # link = '[©Developed by Spidy20](http://github.com/spidy20)'
+    # st.sidebar.markdown(link, unsafe_allow_html=True)
+    img = Image.open('Downloads/SRA_Logo.jpg')
+    img = img.resize((250,250))
+    st.image(img)
+
+    # Create the DB
+    db_sql = """CREATE DATABASE IF NOT EXISTS SRA;"""
+    cursor.execute(db_sql)
+
+    # Create table
+    DB_table_name = 'user_data'
+    table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
+                    (ID INT NOT NULL AUTO_INCREMENT,
+                     Name varchar(100) NOT NULL,
+                     Email_ID VARCHAR(50) NOT NULL,
+                     resume_score VARCHAR(8) NOT NULL,
+                     Timestamp VARCHAR(50) NOT NULL,
+                     Page_no VARCHAR(5) NOT NULL,
+                     Predicted_Field VARCHAR(25) NOT NULL,
+                     User_level VARCHAR(30) NOT NULL,
+                     Actual_skills VARCHAR(300) NOT NULL,
+                     Recommended_skills VARCHAR(300) NOT NULL,
+                     Recommended_courses VARCHAR(600) NOT NULL,
+                     PRIMARY KEY (ID));
+                    """
+    cursor.execute(table_sql)
+    if choice == 'Normal User':
+        # st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Upload your resume, and get smart recommendation based on it."</h4>''',
+        #             unsafe_allow_html=True)
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            # with st.spinner('Uploading your Resume....'):
+            #     time.sleep(4)
+            save_image_path = 'E:/project 113/PDF and DOCX CVs/CVs/'+pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if resume_data:
+                ## Get the whole resume data
+                resume_text = pdf_reader(save_image_path)
+
+                st.header("**Resume Analysis**")
+                st.success("Hello "+ resume_data['name'])
+                st.subheader("**Your Basic info**")
+                try:
+                    st.text('Name: '+resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                    st.text('Resume pages: '+str(resume_data['no_of_pages']))
+                except:
+                    pass
+                cand_level = ''
+                if resume_data['no_of_pages'] == 1:
+                    cand_level = "Fresher"
+                    st.markdown( '''<h4 style='text-align: left; color: #d73b5c;'>You are looking Fresher.</h4>''',unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] == 2:
+                    cand_level = "Intermediate"
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>You are at intermediate level!</h4>''',unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] >=3:
+                    cand_level = "Experienced"
+                    st.markdown('''<h4 style='text-align: left; color: #fba171;'>You are at experience level!''',unsafe_allow_html=True)
+
+                    insert_data(resume_data['name'], resume_data['email'])  
+                    connection.commit()
+            else:
+                st.error('Something went wrong..')
+    else:
+        ## Admin Side
+        st.success('Welcome to Admin Side')
+        # st.sidebar.subheader('**ID / Password Required!**')
+
+        ad_user = st.text_input("Username")
+        ad_password = st.text_input("Password", type='password')
+        if st.button('Login'):
+            if ad_user == 'manoj' and ad_password == '075162870041':
+                st.success("Welcome")
+                # Display Data
+                cursor.execute('''SELECT*FROM user_data''')
+                data = cursor.fetchall()
+                st.header("**User's👨‍💻 Data**")
+                df = pd.DataFrame(data, columns=['ID', 'Name', 'Email', 'Resume Score', 'Timestamp', 'Total Page',
+                                                 'Predicted Field', 'User Level', 'Actual Skills', 'Recommended Skills',
+                                                 'Recommended Course'])
+                st.dataframe(df)
+                st.markdown(get_table_download_link(df,'User_Data.csv','Download Report'), unsafe_allow_html=True)
+                ## Admin Side Data
+                query = 'select * from user_data;'
+                plot_data = pd.read_sql(query, connection)
+
+                ## Pie chart for predicted field recommendations
+                labels = plot_data.Predicted_Field.unique()
+                print(labels)
+                values = plot_data.Predicted_Field.value_counts()
+                print(values)
+                st.subheader("📈 **Pie-Chart for Predicted Field Recommendations**")
+                fig = px.pie(df, values=values, names=labels, title='Predicted Field according to the Skills')
+                st.plotly_chart(fig)
+
+                ### Pie chart for User's👨‍💻 Experienced Level
+                labels = plot_data.User_level.unique()
+                values = plot_data.User_level.value_counts()
+                st.subheader("📈 ** Pie-Chart for User's👨‍💻 Experienced Level**")
+                fig = px.pie(df, values=values, names=labels, title="Pie-Chart📈 for User's👨‍💻 Experienced Level")
+                st.plotly_chart(fig)
+
+
+            else:
+                st.error("Wrong ID & Password Provided")
+run()                   
